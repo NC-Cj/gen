94e0da3df494fd4790a1bc19ab7df58ca2a5ca68
cj <cj@mosynx.com>
2023-08-10 18:08:15 +0800

    fixup! init

--- x.py
+++ x.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import subprocess
 
 import argcomplete
@@ -6,12 +8,18 @@ import fire
 MANAGERS = ["node", "git"]
 REPOSITORIES = {
     "fastapi": {
-        "normal": "",
-        "easy": ""
+        "repository": {
+            "normal": "git@github.com:NC-Cj/gen-fastapi-norm.git",
+            "easy": ""
+        },
+        "name": "gen-fastapi-norm"
     },
     "gin": {
-        "normal": "",
-        "easy": ""
+        "repository": {
+            "normal": "git@github.com:NC-Cj/gen-fastapi-norm.git",
+            "easy": ""
+        },
+        "name": "gen-fastapi-norm"
     }
 }
 
@@ -33,30 +41,58 @@ class Gen(object):
             check_command_exists(value)
 
     @staticmethod
-    def fastapi(t: str):
+    def fastapi(name: str, t: str, orm: bool = False):
         """
         fastapi web Project Scaffolding
-        :param t:  Project type: Options[normal, normal-orm, easy]
-        :return:
-        """
-        if t == 'clone':
 
-        if t == 'clone':
-            # 克隆项目
-            try:
-                result = subprocess.run(['git', 'clone', 'https://github.com/your-repo.git'], capture_output=True,
-                                        text=True)
-                if result.returncode == 0:
-                    print("Project cloned successfully.")
-                else:
-                    print(f"\033[91mERROR:\033[0m Failed to clone project.")
-                print(result.stdout)
-                print(result.stderr)
-            except subprocess.CalledProcessError as e:
-                print(f"\033[91mERROR:\033[0m Failed to clone project: {e}")
-        else:
-            print(t)
-        return
+        :param name:You project Name
+        :type name: str
+        :param t: Project type: Options[normal, normal-orm, easy]
+        :type t: str
+        :param orm: With PrismaORM management database
+        :type orm: bool, default False
+
+        :Example:
+
+        The following examples show how to use the `fastapi` command:
+
+        1. Standard FastAPI web engineering directory:
+           ```
+           gen fastapi -t normal
+           ```
+
+        2. Standard FastAPI web engineering directory with PrismaORM management database:
+           ```
+           gen fastapi -t normal-orm
+           ```
+
+        3. The most concise project:
+           ```
+           gen fastapi -t easy
+           ```
+        """
+
+        url = REPOSITORIES["fastapi"]["repository"][t]
+
+        try:
+            result = subprocess.run(['git', 'clone', url], capture_output=True, text=True)
+            if result.returncode != 0:
+                print(f"\033[91mERROR:\033[0m Failed to clone project.")
+
+            # Get the cloned project directory name
+            pwd = os.getcwd()
+            directory_name = url.split('/')[-1].split('.')[0]
+            project_dir = os.path.join(pwd, directory_name)
+            git_dir = os.path.join(project_dir, '.git')
+
+            if os.path.exists(git_dir):
+                try:
+                    shutil.rmtree(git_dir)
+                except:
+                    pass
+                os.rename(project_dir, os.path.join(pwd, name))
+        except subprocess.CalledProcessError as e:
+            print(f"\033[91mERROR:\033[0m Failed to clone project: {e}")
 
     @staticmethod
     def gin(t: str):
